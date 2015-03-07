059e479d9301677ec88075bd5dcf9daea2d1befb
PyroNyzen <jamescampbell96@Jamess-MBP.lan>
2015-03-07 12:22:12 +1100

    Merge branch 'master' into gh-pages
    Conflicts:
    	index.html
    	params.json

---
+++ js/api.js
@@ -0,0 +1,149 @@
+define(['settings'], function(settings)
+{
+	'use strict';
+	var userData_ = {};
+	var characters_ = {};
+
+	// custom variables
+	
+	var meanings_ = {};
+	var vocabulary_ = {};
+
+	var meaningsList = "";
+	var characterList = "";
+	var vocabularyList = "";
+	
+	var resultsTest = "";
+	var resultsTest2 = "";
+	var resultsTest3 = "";
+
+	// end custom variables
+
+	return {
+		load: function(onComplete) {
+			var key = settings.api_key;
+			if(document.getElementById('vocabOption').checked)
+			{
+				var url = 'https://www.wanikani.com/api/user/' + key + '/vocabulary?callback=define';
+			}
+			else if(document.getElementById('radicalOption').checked)
+			{
+				var url = 'https://www.wanikani.com/api/user/' + key + '/radicals?callback=define';
+			}
+			else
+			{
+				var url = 'https://www.wanikani.com/api/user/' + key + '/kanji?callback=define';
+			}
+
+			require([url], function(data) {
+				if (data.error !== undefined) {
+					var messageElem = document.getElementById('message');
+					messageElem.innerHTML = "API Error: " + data.error.message;
+					return;
+				}
+				userData_ = data.user_information;
+				if(document.getElementById('kanjiOption').checked)
+				{
+					resultsTest = "";
+					data.requested_information.forEach(function (character)
+					{
+						characters_[character.character] = character;
+						characterList += character.character + " ";
+						if(character.user_specific == null)
+						{
+							resultsTest += '<span class="unseen">' + character.character + '</span> ';
+							//console.log("SRS unseen");
+						}
+						else
+						{
+							resultsTest += '<span class="' + character.user_specific.srs + '">' + character.character + '</span> '; 
+							//console.log("SRS " + character.user_specific.srs);
+						}
+					});
+					//console.log(characterList);
+					
+					data.requested_information.forEach(function (meaning)
+					{
+						meanings_[meaning.meaning] = meaning;
+						meaningsList += meaning.meaning + " ";
+					});
+				}
+				else if(document.getElementById('radicalOption').checked)
+				{
+					resultsTest2 = "";
+					data.requested_information.forEach(function (character)
+					{
+						characters_[character.character] = character;
+						characterList += character.character + " ";
+						if(character.character == null)
+						{
+							if(character.user_specific == null)
+							{
+								resultsTest2 += '<span class="image-unseen">' + '<img src="' + character.image + '"/></span>';
+								//console.log("SRS unseen");
+							}
+							else
+							{
+								resultsTest2 += '<span class="' + character.user_specific.srs + '-unseen">' + '<img src="' + character.image + '"/></span>';
+								//console.log("SRS " + character.user_specific.srs);
+							}
+						}
+						else
+						{
+							if(character.user_specific == null)
+							{
+								resultsTest2 += '<span class="unseen">' + character.character + '</span> ';
+								//console.log("SRS unseen");
+							}
+							else
+							{
+								resultsTest2 += '<span class="' + character.user_specific.srs + '">' + character.character + '</span> '; 
+								//console.log("SRS " + character.user_specific.srs);
+							}
+						}
+					});
+				}
+				else
+				{
+					resultsTest3 = "";
+					data.requested_information.general.forEach(function (character)
+					{
+						//vocabulary_[character.character] = character;
+						//vocabularyList += character.character + " ";
+						if(character.user_specific == null)
+						{
+							resultsTest3 += '<span class="unseen">' + character.character + '</span> ';
+							//console.log("SRS unseen");
+						}
+						else
+						{
+							resultsTest3 += '<span class="' + character.user_specific.srs + '">' + character.character + '</span> '; 
+							//console.log("SRS " + character.user_specific.srs);
+						}
+					});
+					//console.log(vocabularyList);
+
+					data.requested_information.general.forEach(function (meaning)
+					{
+						meanings_[meaning.meaning] = meaning;
+						meaningsList += meaning.meaning + " | ";
+					});
+
+				}
+				//console.log(meaningsList);
+				//console.log("Complete Dump " + JSON.stringify(data.requested_information));
+				/*
+				document.getElementById('meaningResults').value = meaningsList;
+				document.getElementById('characterResults').value = characterList;
+				document.getElementById('vocabResults').value = vocabularyList;
+				*/
+				document.getElementById('outputTestResults').innerHTML = resultsTest;
+				document.getElementById('outputTestResults2').innerHTML = resultsTest2;
+				document.getElementById('outputTestResults3').innerHTML = resultsTest3;
+				onComplete();
+			});
+},
+get userData() { return userData_; },
+get characters() { return characters_; }
+};
+});
---
+++ js/canvasdrawer.js
@@ -0,0 +1,94 @@
+define(['settings', 'order'], function(settings, order) {
+
+	var fonts = '"Hiragino Kaku Gothic Pro","Meiryo","ヒラギノ角ゴ Pro W3","メイリオ","Osaka","MS PGothic","ＭＳ Ｐゴシック",sans-serif';
+
+	function getColor(character) {
+		if (character === undefined) {
+			return settings.colors['unseen'];
+		}
+
+		var userData = character.user_specific;
+		if (userData === null) {
+			return settings.colors['unseen'];
+		}
+
+		return settings.colors[userData.srs];
+	}
+
+	function findBestFit(width, height, elementsToFit) {
+		var sqrt = Math.ceil(Math.sqrt(elementsToFit)) + 1;
+
+		var aspectRatio = width / height;
+
+		var y = Math.ceil(sqrt / ((1 + aspectRatio)/2));
+		var x = Math.ceil(elementsToFit / y);
+
+		var charWidth = width / x;
+		var charHeight = height / y;
+		var fontSize = Math.min(charWidth, charHeight);
+		var totalArea = width * height;
+		var coveredArea = elementsToFit * Math.pow(fontSize, 2);
+		var fit = coveredArea / totalArea;
+
+		return {fit: fit, w: x, h: y, fontSize: fontSize, charWidth: charWidth, charHeight: charHeight};
+	}
+
+	return {
+		draw: function(characters) {
+			var canvasElem = document.createElement('canvas');
+			canvasElem.width = settings.width;
+			canvasElem.height = settings.height;
+			
+			var margin = settings.margin;
+			var drawWidth = settings.width - margin.left - margin.right;
+			var drawHeight = settings.height - margin.top - margin.bottom;
+			var aspectRatio = drawWidth / drawHeight;
+
+			var bestFit = findBestFit(drawWidth, drawHeight, order.data.length);
+			
+			var ctx = canvasElem.getContext('2d');
+
+			ctx.fillStyle = settings.colors.background;
+			ctx.fillRect(0, 0, settings.width, settings.height);
+			
+			ctx.font = '' + Math.floor(bestFit.fontSize) + 'px ' + fonts;
+			ctx.textBaseline = 'top';
+
+			var offset = -Math.floor(bestFit.fontSize) * 0.2;
+			// needed to perfectly align the text.
+			
+			var i, x, y, c;
+			for (i = 0; i < order.data.length; i++) {
+				c = order.data[i];
+				//THIS CODE NEEDS EDITING
+				ctx.fillStyle = getColor(characters[c]);
+
+				y = margin.top + Math.floor(i / bestFit.w) * bestFit.charHeight + offset;
+				x = margin.left + (i % bestFit.w) * bestFit.charWidth;
+				ctx.fillText(c, Math.floor(x), Math.floor(y));
+			}
+
+			var dataURL = canvasElem.toDataURL('image/png');
+
+			//document.getElementsByTagName('body')[0].style['background-image'] = "url(" + dataURL + ")";
+			//document.getElementsByTagName('body')[0].style['backgroundImage'] = "url(" + dataURL + ")";
+
+			var img = document.createElement('img');
+			img.src = dataURL;
+			img.width = "100";
+
+			var download_link = document.createElement('a');
+			download_link.id = "download_link"
+			download_link.href = dataURL;
+			download_link.download = "wallpaper.png";
+			download_link.innerHTML = "Download";
+			download_link.target = "_blank";
+
+			var messageElem = document.getElementById('message');
+			messageElem.innerHTML = "";
+			messageElem.appendChild(img);
+			messageElem.appendChild(download_link);
+			messageElem.innerHTML += "<p>Internet explorer users: right click image and save as</p>";
+		}
+	};
+});
